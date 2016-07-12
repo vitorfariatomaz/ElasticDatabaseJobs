e78425bdf5e8cc383c33c30eef5f579a83d6dc01
Vitor Faria Tomaz <vitor.tomaz@outlook.com>
2016-07-12 20:27:56 +0100

    Uninstall added

---
+++ Uninstall Elastic Jobs.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+  Uninstall Azure SQL Database - Elastic Database Jobs
+#>
+
+########################
+# Parameters
+########################
+
+$homeDir = [Environment]::GetFolderPath("UserProfile")
+cd $homeDir
+$FolderName = "ElasticDatabaseJobs"
+
+#It is not recommended to change this value, portal will not detect installation if changed
+$resourceGroupgName = "__ElasticDatabaseJob" 
+
+
+########################
+# Step 0 : Login
+########################
+
+Login-AzureRmAccount
+
+########################
+# Step 1 : Uninstall
+########################
+
+cd $FolderName
+cd .\Microsoft.Azure.SqlDatabase.Jobs.*
+cd tools
+.\UninstallElasticDatabaseJobs.ps1 -ResourceGroupName $resourceGroupgName
+
+
+<# Delete the folder
+cd $homeDir
+Remove-Item $FolderName -Recurse
+#>
